a372a6017f6f19737cb426daddf42467de017038
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-08-27 03:13:04 +0200

    Task 5.2 commit
    Added gifted chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,111 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, View, Text, ImageBackground, TextInput, Button } from 'react-native';
+import { GiftedChat, Bubble, SystemMessage, Day, Time} from 'react-native-gifted-chat';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet } from 'react-native';
+
 
 export default class Chat extends Component {
-    componentDidMount() {
-        let { name } = this.props.route.params;
-        this.props.navigation.setOptions({ title: name });
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
     }
-
-    render() {
-        const { color } = this.props.route.params;
+    renderBubble(props) {
+        let color;
+        if (this.props.route.params.color === '#090C08') color = '#8A95A5'
+        else if (this.props.route.params.color === '#474056') color = '#B9C6AE'
+        else if (this.props.route.params.color === '#8A95A5') color = '#B9C6AE'
+        else if (this.props.route.params.color === '#B9C6AE') color = '#474056'
 
         return (
-            <ScrollView style={{ backgroundColor: color }}>
-                <View style={styles.container}>
-                    <Text style={styles.text}>Chat Screen</Text>
-                </View>
-            </ScrollView>
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: 'color'
+                    }
+                }}
+            />
+        )
+    }
+
+    renderDay(props) {
+        return <Day {...props} textStyle={{ color: 'white' }} />
+    }
+
+    renderTime(props) {
+        return (
+            <Time
+                {...props}
+                timeTextStyle={{
+                    left: {
+                        color: 'black',
+                    },
+                    right: {
+                        color: 'white',
+                    },
+                }}
+            />
+        );
+    };
+
+    renderSystemMessage(props) {
+        return 
+            <SystemMessage {...props} textStyle={{ color: 'white' }} />
+        
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+ 
+    render() {
+        const { color } = this.props.route.params;
+    return (
+        <View style={[styles.container, { backgroundColor: color }]}>
+            <GiftedChat
+                renderBubble={this.renderBubble.bind(this)}
+                renderDay={this.renderDay.bind(this)}
+                renderTime={this.renderTime.bind(this)}
+                renderSystemMessage={this.renderSystemMessage.bind(this)}
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+        </View>
         );
     }
+
+componentDidMount() {
+    let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+    this.setState({
+        messages: [
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: '${name} has entered the chat',
+                createdAt: new Date(),
+                system: true,
+            },
+        ],
+    })
+}
 }
 
 const styles = StyleSheet.create({
